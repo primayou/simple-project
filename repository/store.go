9b4d994a56e848de026a77c0904145a412553dd3
primayou <prima.yudantra@you.co>
2022-04-17 22:25:21 +0700

    add repository

---
+++ repository/store.go
@@ -0,0 +1,15 @@
+package repository
+
+import "database/sql"
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
